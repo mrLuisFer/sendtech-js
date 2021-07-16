72fbfbcf02013f9fa33d2e7a38a7413b0ab10689
mrLuisFer <mrluisfeer@gmail.com>
2021-07-15 22:34:12 -0500

    feat: añadi que se pudiera interactuar con los buttons

--- src/commands/tests/testButton.js
+++ src/commands/tests/testButton.js
@@ -1,27 +1,27 @@
 const { MessageButton, MessageActionRow } = require('discord-buttons')
 
 const testButton = (msg, client) => {
-  const button = new MessageButton()
+  const button2Id = 'second_button_function'
+
+  const button2 = new MessageButton()
     .setURL('https://discord.gg/4FUtbhatAg')
     .setStyle('url')
     .setLabel('Vayamos al servidor! ;D')
     .setEmoji('🌟')
 
-  const button2 = new MessageButton()
-    .setStyle('red')
-    .setLabel('Copiar enlace!')
-    .setID('second_button_function')
+  const button = new MessageButton().setStyle('red').setLabel('Copiar enlace!').setID(button2Id)
 
   const row = new MessageActionRow().addComponents(button, button2)
 
   msg.channel.send('Invita a tus amigos al servidor! :D https://discord.gg/4FUtbhatAg', row)
 
-  // client.on('clickButton', async (button) => {
-  //   console.log(`Ejecutando ${button.id}`)
-
-  //   let s = button.send(`Tocaste el ${button.id}`)
-  //   msg.channel.send(s)
-  // })
+  client.on('clickButton', async (button) => {
+    if (button.id === button2Id) {
+      console.log(`Ejecutando ${button.id}`)
+      await button.channel.send('Clickeado')
+    }
+    button.defer()
+  })
 }
 
 module.exports = testButton
